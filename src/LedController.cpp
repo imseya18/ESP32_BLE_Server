757f1a5a3f07d7f5e3790ed527ff85ca43caf10e
imseya18 <imseya68@gmail.com>
2025-07-08 16:05:10 +0200

    refactor(log): add logs

--- src/LedController.cpp
+++ src/LedController.cpp
@@ -1,5 +1,7 @@
 #include "LedController.hpp"
+#include "esp_log.h"
 
+static const char* TAG = "LedController";
 LedController::LedController(CRGBArray<LED_COUNT> &ledsRef, std::vector<CRGBSet> &segmentsRef) : _leds(ledsRef), _segments(segmentsRef), _saved_brightness(255)
 {
 }
@@ -11,9 +13,10 @@ LedController::~LedController()
 }
 
 void LedController::setLedColor(u_int8_t segment_index, CRGB rgbval)
-{
+{   
      _segments[segment_index].fill_solid(rgbval);
      FastLED.show();
+     ESP_LOGD(TAG, "Color set to: R:%i G:%i B:%i on segment: %i", rgbval.r ,rgbval.g, rgbval.b , segment_index);
 }
 
 void LedController::setBrightness(u_int8_t brightness_value)
@@ -21,14 +24,17 @@ void LedController::setBrightness(u_int8_t brightness_value)
     _saved_brightness = brightness_value;
     FastLED.setBrightness(brightness_value);
     FastLED.show();
+    ESP_LOGD(TAG, "Brightness set to: %i", brightness_value);
 }
 
 void LedController::setLedsOn()
 {
     FastLED.setBrightness(_saved_brightness);
+    FastLED.show();
 }
 
 void LedController::setLedsOff()
 {
     FastLED.setBrightness(0);
+    FastLED.show();
 }
--- src/StripCallBacks.cpp
+++ src/StripCallBacks.cpp
@@ -1,7 +1,9 @@
 #include "StripCallBacks.hpp"
 #include "Constant.hpp"
 #include "FastLED.h"
+#include "esp_log.h"
 
+static const char* TAG = "SripCB";
 
 StripCallBacks::StripCallBacks(LedController& led_controller) : _led_controller(led_controller)
 {
@@ -18,47 +20,53 @@ void StripCallBacks::onWrite(NimBLECharacteristic* payload, NimBLEConnInfo& conn
         const uint8_t * raw_data = payload_value.data();
         if(!is_valide_payload(&payload_value))
         {
-            Serial.printf("Not a valide Payload!\n");
+            ESP_LOGW(TAG, "Invalid payload");
             return;
         }
-        for(int i = 0; i < payload_size; i++)
-        {
-            Serial.printf("%#02X,", raw_data[i]);
-        }
-        Serial.printf("\n");
         switch (raw_data[COMMAND_POS])
         {
             case LED_COLOR: {
-                u_int8_t segment_index =raw_data[SEGMENT_POS];
+                ESP_LOGD(TAG ,"LED_COLOR Command trigered");
+                u_int8_t segment_index = raw_data[SEGMENT_POS];
                 CRGB rgbval(raw_data[RED_VALUE_POS],raw_data[GREEN_VALUE_POS], raw_data[BLUE_VALUE_POS]);
                 _led_controller.setLedColor(segment_index, rgbval);
                 break;
             }
             case BRIGHTNESS: {
-                Serial.printf("BRIGHTNESS Command trigered");
+                ESP_LOGD(TAG ,"BRIGHTNESS Command trigered");
                 _led_controller.setBrightness(raw_data[BRIGHTNESS_POS]);
                 break;
             }
             case LED_ON: {
+                ESP_LOGD(TAG ,"LED_ON Command trigered");
                 _led_controller.setLedsOn();
+                break;
             }
             case LED_OFF:{
+                ESP_LOGD(TAG ,"LED_OFF Command trigered");
                 _led_controller.setLedsOff();
+                break;
             }
             default:{
-                Serial.printf("Not a Valide Command");
+                ESP_LOGW(TAG ,"Not a valid command");
                 break;
             }
         }
 }
 
 bool StripCallBacks::is_valide_payload(const NimBLEAttValue* payload) const {
-        if (payload->length() != PAYLOAD_SIZE)
+        if (payload->length() != PAYLOAD_SIZE){
+            ESP_LOGD(TAG, "Invalid payload length=%u", payload->length());
             return false;
+        }
         const uint8_t* raw_data = payload->data();
-        if(raw_data[0] != START_FLAG || raw_data[PAYLOAD_SIZE - 1] != END_FLAG)
+        if(raw_data[0] != START_FLAG || raw_data[PAYLOAD_SIZE - 1] != END_FLAG){
+            ESP_LOGD(TAG, "Invalid payload Flag");
             return false;
-        if(raw_data[SEGMENT_POS] < 0 || raw_data[SEGMENT_POS] > SEGMENT_NUMBER)
+        }
+        if(raw_data[SEGMENT_POS] < 0 || raw_data[SEGMENT_POS] > SEGMENT_NUMBER){
+            ESP_LOGD(TAG, "Invalid Segment Value");
             return false;
+        }
         return true;
 }
